05dc6112d282b9c6e6411f11141ce9b529e0f433
plant <bmechtley@gmail.com>
2013-09-13 03:57:25 +0100

    circumcircle

--- coordinates.py
+++ coordinates.py
@@ -11,6 +11,7 @@ for display. All routines automatically normalize barycentric coordinates.
 
 from itertools import izip
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as pp
 
 def project_pointline(p, a, b):
@@ -216,3 +217,210 @@ def baryedges(coords, sidecoords=None):
         return np.array([e[i1, (i1, i2)] for i1, i2 in pairs])
     
     return e
+
+def circumcircle(a, b, c):
+    """
+    Return the center coordinates of a circle that circumscribes an input triangle defined by 2D vertices a, b, and c.
+    
+    Args:
+        a ((number, number) or np.ndarray): first vertex of the input triangle.
+        b ((number, number) or np.ndarray): second vertex of the input triangle.
+        c ((number, number) or np.ndarray): third vertex of the input triangle.
+        
+    Returns:
+        Center coordinates of circumscribing circle in form (x, y).
+    """
+    
+    ax, ay = a
+    bx, by = b
+    cx, cy = c
+    ax2, ay2, bx2, by2, cx2, cy2 = [d ** 2 for d in [ax, ay, bx, by, cx, cy]]
+    
+    d = 2 * (ax * (by - cy) + bx * (cy - ay) + cx * (ay - by))
+    
+    ux = (
+        (ax2 + ay2) * (by - cy) +
+        (bx2 + by2) * (cy - ay) +
+        (cx2 + cy2) * (ay - by)
+    ) / d
+    
+    uy = (
+        (ax2 + ay2) * (cx - bx) +
+        (bx2 + by2) * (ax - cx) +
+        (cx2 + cy2) * (bx - ax)
+    ) / d
+    
+    return ux, uy
+
+def voronoi(x, y):
+    """
+    Return a list of voronoi cells for a collection of points.
+
+    Args:
+        x (list or np.ndarray): list of coordinates' x components.
+        y (list or np.ndarray): list of coordinates' y components.
+    
+    Returns:
+        (cells, triangles), where cells is a list of voronoi cells, each once containing a list of two-dimensional
+        points; and triangles is a list of the triangles from a Delaunay triangulation.
+    """
+    
+    p = np.array(zip(x, y))
+    d = mpl.tri.Triangulation(x, y)
+    t = d.triangles
+    n = t.shape[0]
+    
+    # Get circle for each triangle, center will be a voronoi cell point. 
+    cells = [[] for i in range(x.size)]                     # [[]] * x.size will have the same object at each index.
+    
+    for i in range(n):
+        v = [p[t[i,j]] for j in range(3)]
+        pt = circumcircle(v[0], v[1], v[2])
+        
+        cells[t[i,0]].append(pt)
+        cells[t[i,1]].append(pt)
+        cells[t[i,2]].append(pt)
+    
+    # Reordering cell p in trigonometric way
+    for i, cell in enumerate(cells):
+        xy = np.array(cell)
+        order = np.argsort(np.arctan2(xy[:,1] - y[i], xy[:,0] - x[i]))
+        
+        cell = xy[order].tolist()
+        cell.append(cell[0])
+        
+        cells[i] = cell
+    
+    return cells
+
+
+def baryplot(values, points=None, labels='abc', cmap=mpl.cm.BrBG, clabel='', vmin=None, vmax=None):
+    """
+    Create a triangular voronoi cell pseudocolor plot. Create a voronoi diagram for each coordinate (points) within the
+    triangle and color each cell according to its value (values).
+    
+    Args:
+        values (list or np.ndarray): list of scalar values for each barycentric point.
+        points (list or np.ndarray): list of three-parameter barycentric points.
+        labels (list): list of three label strings, one for each side of the triangle.
+        cmap (matplotlib.colors.Colormap): colormap for pseudocolor plot.
+        clabel (str): colorbar label for values
+        vmin (number): minimum value for coloring. If None, use minimum of the input values.
+        vmax (number): maximum value for coloring. If None, use maximum of the input values.
+    """
+    
+    if points is None: points = []
+    
+    vmin = vmin if vmin is not None else np.amin(values)
+    vmax = vmax if vmax is not None else np.amax(values)
+    
+    p = bary2cart(points) if len(points) else bary2cart(lattice(3))
+    norm = mpl.colors.Normalize(vmin=vmin, vmax=vmax)
+    mappable = mpl.cm.ScalarMappable(norm=norm, cmap=cmap)
+    colors = mappable.to_rgba(values)
+    
+    #values = (values - np.amin(values)) / (np.amax(values) - np.amin(values))
+    cells = voronoi(p[:,0], p[:,1])
+    
+    xmin, xmax, xavg = np.amin(p[:,0]), np.amax(p[:,0]), np.mean(p[:,0])
+    ymin, ymax, yavg = np.amin(p[:,1]), np.amax(p[:,1]), np.mean(p[:,1])
+    
+    s60, c60 = np.sin(np.pi / 3.), np.cos(np.pi / 3.)
+    s30, c30 = np.sin(np.pi / 6.), np.cos(np.pi / 6.)
+    
+    # Start drawing.
+    ax = pp.gca()
+    
+    # Clipping triangle for the voronoi patches.
+    clip = mpl.patches.Polygon([
+        (xmin, ymin), (xmax, ymin), (xavg, ymax),
+    ], transform=ax.transData)
+    
+    # Draw voronoi patches.
+    for i, cell in enumerate(cells):
+        codes = [mpl.path.Path.MOVETO] \
+            + [mpl.path.Path.LINETO] * (len(cell) - 2) \
+            + [mpl.path.Path.CLOSEPOLY]
+        
+        pth = mpl.path.Path(cell, codes)
+        
+        patch = mpl.patches.PathPatch(
+            pth, 
+            zorder=-1, 
+            facecolor=colors[i],
+            clip_path=clip,
+            edgecolor='none'
+        )
+        
+        ax.add_patch(patch)
+    
+    # Add barycentric labels for vertices.
+    ax.text(xmin - .0125, ymin - .02, '$(0,1,0)$', ha='right', va='center')
+    ax.text(xmax + .0125, ymin - .02, '$(0,0,1)$', ha='left', va='center')
+    ax.text(xavg, ymax + .035, '$(1,0,0)$', ha='center', va='bottom')
+    
+    # Labels.
+    ax.text(
+        xavg + c30 * .35, yavg + s30 * .35, 
+        labels[2], ha='center', va='center', rotation=-60
+    )
+    
+    ax.text(
+        xavg, ymin - .05, 
+        labels[1], ha='center', va='top'
+    )
+    
+    ax.text(
+        xavg - c30 * .35, yavg + s30 * .35, 
+        labels[0], ha='center', va='center', rotation=60
+    )
+    
+    arrowopts = dict(
+        width=.00125,
+        frac=.0125,
+        headwidth=.01,
+        transform=ax.transData
+    )
+    
+    fig = pp.gcf()
+    
+    # Arrows along edges.
+    ax.add_patch(mpl.patches.YAArrow(
+        fig,
+        (xmin - c60 * .025, ymin + s60 * .025),
+        (xavg - c60 * .025, ymax + s60 * .025),
+        **arrowopts
+    ))
+    
+    ax.add_patch(mpl.patches.YAArrow(
+        fig,
+        (xmax, ymin - .025),
+        (xmin, ymin - .025),
+        **arrowopts
+    ))
+    
+    ax.add_patch(mpl.patches.YAArrow(
+        fig,
+        (xavg + c60 * .025, ymax + s60 * .025),
+        (xmax + c60 * .025, ymin + s60 * .025),
+        **arrowopts
+    ))
+    
+    # Make axes equal, get rid of border.
+    pp.axis('equal')
+    ax.axis([
+        xmin - c60 * .2, xmax + c60 * .2, 
+        ymin - s60 * .2, ymax + s60 * .2
+    ])
+    pp.axis('off')
+    
+    cax, kw = mpl.colorbar.make_axes(ax, orientation='vertical', shrink=0.7)
+    cb = mpl.colorbar.ColorbarBase(
+        cax, 
+        cmap=cmap,
+        norm=norm,
+        orientation='vertical',
+        ticks=np.linspace(vmin, vmax, 5)
+    )
+    
+    cb.set_label(clabel)

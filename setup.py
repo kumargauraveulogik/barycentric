d4da7e04b51f5c893713f6e996e9674441698099
plant <bmechtley@gmail.com>
2013-09-24 11:20:50 +0100

    actual module

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='barycentric',
+    version='0.1',
+    description='various utilities for dealing with barycentric coordinates in numpy and matplotlib.',
+    author='Brandon Mechtley',
+    author_email='bmechtley@gmail.com',
+    url='http://github.com/plant/barycentric/',
+    packages=['barycentric']
+)
+

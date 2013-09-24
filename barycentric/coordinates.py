aaea3aeb410455ebe662f7a00a959665f3396a94
plant <bmechtley@gmail.com>
2013-09-24 12:27:46 +0100

    formatting improvements

--- barycentric/coordinates.py
+++ barycentric/coordinates.py
@@ -1,12 +1,12 @@
 '''
 coordinates.py
-natural-mixer
 
 2012 Brandon Mechtley
 Arizona State University
 
-Various tools for working with barycentric coordinates of arbitrary dimension including routines
-for display. All routines automatically normalize barycentric coordinates.
+Various tools for working with barycentric coordinates of arbitrary dimension
+including routines for display. All routines automatically normalize 
+barycentric coordinates.
 '''
 
 import numpy as np
@@ -14,24 +14,35 @@ import matplotlib as mpl
 import matplotlib.pyplot as pp
 
 def project_pointline(p, a, b):
-    '''Euclidean projection of a point onto a line segment.
-        p: np.ndarray
-            point of arbitrary dimensionality.
-        a: np.ndarray
-            first endpoint of the line segment.
-        b: np.ndarray
-            second endpoint of the line segment.'''
+    '''
+    Euclidean projection of a point onto a line segment.
+
+    Args:
+        p (np.ndarray): point of arbitrary dimensionality.
+        a (np.ndarray): first endpoint of the line segment.
+        b (np.ndarray): second endpoint of the line segment.
+
+    Returns:
+        (1, 2) np.ndarray of the projected cartesian coordinates.
+    '''
 
     return a + (np.dot(p - a, b - a) / np.dot(b - a, b - a)) * (b - a)
 
 def bary2cart(bary, corners=None):
-    '''Convert barycentric coordinates to cartesian coordinates given the cartesian coordinates of
-    the corners.
-        bary: np.ndarray
-            barycentric coordinates to convert. If this matrix has multiple rows, each row is
-            interpreted as an individual coordinate to convert.
-        corners: np.ndarray
-            cartesian coordinates of the corners.'''
+    '''
+    Convert barycentric coordinates to cartesian coordinates given the
+    cartesian coordinates of the corners.
+
+    Args:
+        bary (np.ndarray): barycentric coordinates to convert. If this matrix
+            has multiple rows, each row is interpreted as an individual
+            coordinate to convert.
+        corners (np.ndarray): cartesian coordinates of the corners.
+
+    Returns:
+        2-column np.ndarray of cartesian coordinates for each barycentric
+        coordinate provided.
+    '''
 
     if corners == None:
         corners = polycorners(bary.shape[-1])
@@ -46,14 +57,21 @@ def bary2cart(bary, corners=None):
     return cart
 
 def lattice(ncorners=3, sides=False):
-    '''Create a lattice of linear combinations of barycentric coordinates with ncorners corners.
-    This lattice is constructed from the corners, the center point between them, points between the
-    corners and the center, and pairwise combinations of the corners and the center.
-        ncorners: int, optional
-            number of corners of the boundary polygon (default 3).
-        sides: bool, optional
-            whether or not to include pairwise combinations of the corners (i.e. sides) in the
-            lattice (default False).'''
+    '''
+    Create a lattice of linear combinations of barycentric coordinates with 
+    ncorners corners. This lattice is constructed from the corners, the center
+    point between them, points between the corners and the center, and pairwise
+    combinations of the corners and the center.
+
+    Args:
+        ncorners (int, optional): number of corners of the boundary polygon 
+            (default 3).
+        sides (bool, optional): whether or not to include pairwise combinations
+            of the corners (i.e. sides) in the lattice (default False).
+
+    Returns:
+        np.ndarray of barycentric coordinates for each point in the lattice.
+    '''
 
     # 1. Corners.
     coords = list(np.identity(ncorners))
@@ -77,14 +95,21 @@ def lattice(ncorners=3, sides=False):
             for j in range(i + 1, ncorners):
                 coords.append((coords[i] + coords[j]) / 2)
 
-    # 5. Return unique coordinates (some duplicates using this method with e.g. ncorners=2)
+    # 6. Return unique coordinates (some duplicates using this method with
+    # e.g. ncorners=2)
     return np.array(list(set(tuple(c) for c in coords)))
 
 def polycorners(ncorners=3):
-    '''Return 2D cartesian coordinates of a regular convex polygon of a specified number of
-    corners.
-        ncorners: int, optional
-            number of corners for the polygon (default 3).'''
+    '''
+    Return 2D cartesian coordinates of a regular convex polygon of a specified
+    number of corners.
+
+    Args:
+        ncorners (int, optional) number of corners for the polygon (default 3).
+
+    Returns:
+        (ncorners, 2) np.ndarray of cartesian coordinates of the polygon.
+    '''
 
     center = np.array([0.5, 0.5])
     points = []
@@ -98,19 +123,22 @@ def polycorners(ncorners=3):
     return np.array(points)
 
 def verttext(pt, txt, center=[.5,.5], dist=1./15, color='red'):
-    '''Display a text label for a vertex with respect to the center. The text will be a certain
-    distance from the specified vertex in the direction of the vector extending from the center
-    point.
-        pt: np.ndarray
-            two-dimensional array of cartesian coordinates of the point to label.
-        txt: str
-            text to display. Text will be horizontally and vertically centered around pt.
-        center: np.ndarray, optional
-            reference center point used for arranging the text around pt (default [.5, .5]).
-        dist: float, optional
-            distance between point and the text (default 1./15).
-        color: str, optional
-            matplotlib color of the text (default 'red').'''
+    '''
+    Display a text label for a vertex with respect to the center. The text
+    will be a certain distance from the specified vertex in the direction of
+    the vector extending from the center point.
+
+    Args:
+        pt (np.ndarray): two-dimensional array of cartesian coordinates of the
+            point to label.
+        txt (str): text to display. Text will be horizontally and vertically
+            centered around pt.
+        center (np.ndarray, optional): reference center point used for 
+            arranging the text around pt (default [.5, .5]).
+        dist (float, optional): distance between point and the text
+            (default 1./15).
+        color (str, optional): matplotlib color of the text (default 'red').
+    '''
 
     vert = pt - center
     s = np.sum(np.abs(vert))
@@ -131,15 +159,28 @@ def verttext(pt, txt, center=[.5,.5], dist=1./15, color='red'):
         color=color
     )
 
-def polyshow(coords, color=None, label=None, labelvertices=False, polycolor=None, lines=[]):
-    '''Plot a regular convex polygon surrounding one or more barycentric coordinates within the
-    it. Vertices and corners will be labeled sequentially starting at 0.
-        coords: np.ndarray or list
-            one or more barycentric coordinates of equal arbitrary dimension. The dimensionality of
-            the coordinates will correspond to the number of vertices of the polygon that is drawn.
-        color: str or list, optional
-             color in which to draw the coords. If color is a list of the same length as coords,
-             each entry will correspond to the respective coordinates.'''
+def polyshow(
+    coords, 
+    color=None, 
+    label=None, 
+    labelvertices=False, 
+    polycolor=None, 
+    lines=[]
+):
+    '''
+    Plot a regular convex polygon surrounding one or more barycentric 
+    coordinates within the it. Vertices and corners will be labeled
+    sequentially starting at 0.
+
+    Args:
+        coords (np.ndarray or list): one or more barycentric coordinates of 
+            equal arbitrary dimension. The dimensionality of the coordinates 
+            will correspond to the number of vertices of the polygon that is
+            drawn.
+        color (str or list, optional): color in which to draw the coords. If
+            color is a list of the same length as coords, each entry will 
+            correspond to the respective coordinates.
+    '''
 
 
     # Defaults.
@@ -160,8 +201,13 @@ def polyshow(coords, color=None, label=None, labelvertices=False, polycolor=None
 
     # Cartesian coordinates of the vertices of the polygon and each point.
     corners = polycorners(d)
-    cart = [np.sum([c * cnr for c, cnr in zip(coord, corners)], axis=0) for coord in coords]
-    cart = np.array(cart)
+    cart = np.array([
+        np.sum([
+            c * cnr 
+            for c, cnr in zip(coord, corners)
+        ], axis=0) 
+        for coord in coords
+    ])
 
     # Figure/axes setup.
     f = pp.figure(frameon=False)
@@ -188,27 +234,39 @@ def polyshow(coords, color=None, label=None, labelvertices=False, polycolor=None
     return f
 
 def baryedges(coords, sidecoords=None):
-    '''Return an array of barycentric coordinates corresponding to the closest point on each edge
-    of the respective polygon.
-        coords: np.ndarray
-            input coordinates.'''
+    '''
+    Return an array of barycentric coordinates corresponding to the closest
+    point on each edge of the respective polygon.
 
-    # There ought to be an easier way to do this without switching out of barycentric coordinates,
-    # but after obsessing over it for an entire day, I'll have to work on that later.
+    Args:
+        coords (np.ndarray): input coordinates.
+
+    Returns:
+        np.ndarray of barycentric coordinates.
+    '''
+
+    # There ought to be an easier way to do this without switching out of
+    # barycentric coordinates, but after obsessing over it for an entire day,
+    # I'll have to work on that later.
 
     d = len(coords)                     # number of edges.
-    corners = polycorners(d)            # cartesian corners of the polygon.
-    cart = bary2cart(coords, corners)   # cartesian coordinates of the input point.
+    corners = polycorners(d)            # cart. corners of the polygon.
+    cart = bary2cart(coords, corners)   # cart. coordinates of the input point.
 
-    e = np.zeros((d, d))                # final bary. coords. for projection to each side.
+    e = np.zeros((d, d))                # bary. coords. for proj. to each side.
 
     # Pairs of corners that form sides (01, 12, 20 for a triange).
     pairs = zip(np.arange(d), np.roll(np.arange(d), -1))
 
-    # Project the point onto each edge in cartesian space, put it back into bary. coords.
+    # Proj. the pt onto each edge in cart. space, put it back into bary. coords.
     for i1, i2 in pairs:
         proj = project_pointline(cart, corners[i1], corners[i2])
-        distances = np.array([np.linalg.norm(corners[a] - proj, 2) for a in [i1, i2]])
+        
+        distances = np.array([
+            np.linalg.norm(corners[a] - proj, 2) 
+            for a in [i1, i2]
+        ])
+
         sidebary = 1.0 - distances / np.sum(distances)
         e[i1, (i1, i2)] = sidebary
 
@@ -218,8 +276,9 @@ def baryedges(coords, sidecoords=None):
     return e
 
 def circumcircle(a, b, c):
-    """
-    Return the center coordinates of a circle that circumscribes an input triangle defined by 2D vertices a, b, and c.
+    '''
+    Return the center coordinates of a circle that circumscribes an input
+    triangle defined by 2D vertices a, b, and c.
 
     Args:
         a ((number, number) or np.ndarray): first vertex of the input triangle.
@@ -228,7 +287,7 @@ def circumcircle(a, b, c):
 
     Returns:
         Center coordinates of circumscribing circle in form (x, y).
-    """
+    '''
 
     ax, ay = a
     bx, by = b
@@ -252,7 +311,7 @@ def circumcircle(a, b, c):
     return ux, uy
 
 def voronoi(x, y):
-    """
+    '''
     Return a list of voronoi cells for a collection of points.
 
     Args:
@@ -260,9 +319,10 @@ def voronoi(x, y):
         y (list or np.ndarray): list of coordinates' y components.
 
     Returns:
-        (cells, triangles), where cells is a list of voronoi cells, each once containing a list of two-dimensional
-        points; and triangles is a list of the triangles from a Delaunay triangulation.
-    """
+        (cells, triangles), where cells is a list of voronoi cells, each once
+        containing a list of two-dimensional points; and triangles is a list of
+        the triangles from a Delaunay triangulation.
+    '''
 
     p = np.array([a for a in zip(x, y)])
     d = mpl.tri.Triangulation(x, y)
@@ -270,7 +330,7 @@ def voronoi(x, y):
     n = t.shape[0]
     
     # Get circle for each triangle, center will be a voronoi cell point.
-    cells = [[] for i in range(x.size)]                     # [[]] * x.size will have the same object at each index.
+    cells = [[] for i in range(x.size)]
 
     for i in range(n):
         v = [p[t[i,j]] for j in range(3)]
@@ -293,20 +353,34 @@ def voronoi(x, y):
     return cells
 
 
-def baryplot(values, points=None, labels='abc', cmap=mpl.cm.BrBG, clabel='', vmin=None, vmax=None):
-    """
-    Create a triangular voronoi cell pseudocolor plot. Create a voronoi diagram for each coordinate (points) within the
-    triangle and color each cell according to its value (values).
+def baryplot(
+    values, 
+    points=None, 
+    labels='abc', 
+    cmap=mpl.cm.BrBG, 
+    clabel='', 
+    vmin=None, 
+    vmax=None
+):
+    '''
+    Create a triangular voronoi cell pseudocolor plot. Create a voronoi diagram
+    for each coordinate (points) within the triangle and color each cell
+    according to its value (values).
 
     Args:
-        values (list or np.ndarray): list of scalar values for each barycentric point.
-        points (list or np.ndarray): list of three-parameter barycentric points.
-        labels (list): list of three label strings, one for each side of the triangle.
+        values (list or np.ndarray): list of scalar values for each barycentric 
+            point.
+        points (list or np.ndarray): list of three-parameter barycentric 
+            points.
+        labels (list): list of three label strings, one for each side of the
+            triangle.
         cmap (matplotlib.colors.Colormap): colormap for pseudocolor plot.
         clabel (str): colorbar label for values
-        vmin (number): minimum value for coloring. If None, use minimum of the input values.
-        vmax (number): maximum value for coloring. If None, use maximum of the input values.
-    """
+        vmin (number): minimum value for coloring. If None, use minimum of the
+            input values.
+        vmax (number): maximum value for coloring. If None, use maximum of the
+            input values.
+    '''
 
     if points is None: points = []
 
